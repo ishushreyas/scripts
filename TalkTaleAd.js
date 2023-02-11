27de3d80aced90e7ee37ed8d514d8cf63228bf5b
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2023-02-11 20:52:27 +0530

    Create TalkTaleAd.js

---
+++ TalkTaleAd.js
@@ -0,0 +1,9 @@
+var fonts = document.createElement('link');
+fonts.rel = 'stylesheet';
+fonts.href = 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Nunito:wght@700&family=Patrick+Hand&family=Quicksand:wght@700&display=swap';
+var theme = document.createElement('link');
+theme.rel = 'stylesheet';
+theme.href = 'https://talktale.web.app/theme.css';
+var icons = document.createElement('link');
+icons.rel = 'stylesheet';
+icons.href = 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200';
