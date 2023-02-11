cd675977e9324c1cd11f2a2ec84931e0ad99c3f9
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2023-02-11 22:33:22 +0530

    Update talktalead.js

--- talktalead.js
+++ talktalead.js
@@ -4,7 +4,7 @@ style.href = 'https://ishushreyas.github.io/scripts/style.css';
 document.body.appendChild(style);
 var iframe = document.createElement('iframe');
 iframe.frameBorder= 'none';
-iframe.height = '70%';
+iframe.height = '60%';
 iframe.width = '100%';
 iframe.src='https://chat-9a308.web.app?platform='+ parent.document.URL;
 var head = document.createElement('h2');
@@ -13,7 +13,7 @@ head.setAttribute('class', 'secondary-text');
 var cont = document.createElement('div');
 document.body.appendChild(cont);
 cont.setAttribute('class', 'surface-variant');
-cont.style = 'margin: 12px; padding: 12px; border-radius:22px; text-align: center; box-shadow:3px 3px 9px rgb(0,0,0,0.3); max-height: 70%;';
+cont.style = 'margin: 12px; padding: 12px; border-radius:22px; text-align: center; box-shadow:3px 3px 9px rgb(0,0,0,0.3); max-height: 70%; overflow: scroll;';
 cont.appendChild(head);
 cont.appendChild(iframe);
 
