b4e35379a01bdedca7379ff9bebb0a93167d2da9
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2023-02-11 22:20:55 +0530

    Update talktalead.js

--- talktalead.js
+++ talktalead.js
@@ -7,8 +7,11 @@ iframe.frameBorder= 'none';
 iframe.height = '80%';
 iframe.width = '100%';
 iframe.src='https://chat-9a308.web.app?platform='+ parent.document.URL;
+var head = document.createElement('h2');
+head.innerHTML = 'Comments';
 var cont = document.createElement('div');
-document.getElementById('commentBox').appendChild(cont);
+document.body.appendChild(cont);
 cont.setAttribute('class', 'surface-variant cont');
+cont.appendChild(head);
 cont.appendChild(iframe);
 
